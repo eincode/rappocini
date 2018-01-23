02d495a4b6e015154ebe20d9a68619aa5d47abb4
Ananta Pratama <deras.hujan15@gmail.com>
2018-01-23 20:52:54 +0800

    Init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 80
 
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
