93393933b83e3f21f60b37255dde7388b377ccc3
Ananta Pratama <anantapratama@Anantas-MacBook-Pro.local>
2019-12-20 10:23:54 +0700

    Update new versions

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const sheets = require('./sheets')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const fs = require('fs')
-const readline = require('readline')
-const googleAuth = require('google-auth-library')
+const sheets = require('./sheets');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const readline = require('readline');
+const googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/'
-const TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json'
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const TOKEN_DIR =
+  (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) +
+  '/.credentials/';
+const TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
 
-var credentials
+var credentials;
 // Load client secrets from local file
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-	if (err) {
-		console.log('Error loading client secret file: ' + err)
-		return
-	}
-	//Authorize a client with the loaded credentials, then call the Google Sheets API
-	credentials = JSON.parse(content)
-	// authorize(JSON.parse(content), sheets.writeRating)
-})
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  //Authorize a client with the loaded credentials, then call the Google Sheets API
+  credentials = JSON.parse(content);
+  // authorize(JSON.parse(content), sheets.writeRating)
+});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -32,21 +34,21 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback, type, star) {
-	let clientSecret = credentials.installed.client_secret
-	let clientId = credentials.installed.client_id
-	let redirectUrl = credentials.installed.redirect_uris[0]
-	let auth = new googleAuth()
-	let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl)
+  let clientSecret = credentials.installed.client_secret;
+  let clientId = credentials.installed.client_id;
+  let redirectUrl = credentials.installed.redirect_uris[0];
+  let auth = new googleAuth();
+  let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-	//Check if we previously have stored a token
-	fs.readFile(TOKEN_PATH, function(err, token) {
-		if (err) {
-			getNewToken(oauth2Client, callback)
-		} else {
-			oauth2Client.credentials = JSON.parse(token)
-			callback(oauth2Client, type, star)
-		}
-	})
+  //Check if we previously have stored a token
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client, type, star);
+    }
+  });
 }
 
 /**
@@ -58,27 +60,27 @@ function authorize(credentials, callback, type, star) {
  *     client.
  */
 function getNewToken(oauth2Client, callback) {
-	let authUrl = oauth2Client.generateAuthUrl({
-		access_type: 'offline',
-		scope: SCOPES
-	})
-	console.log('Authorize this app by visiting this url: ', authUrl)
-	let r1 = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	})
-	r1.question('Enter the code from that page here: ', function(code) {
-		r1.close()
-		oauth2Client.getToken(code, function(err, token) {
-			if (err) {
-				console.log('Error while trying to retreive access token', err)
-				return
-			}
-			oauth2Client.credentials = token
-			storeToken(token)
-			callback(oauth2Client)
-		})
-	})
+  let authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  let r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  r1.question('Enter the code from that page here: ', function(code) {
+    r1.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retreive access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
 }
 
 /**
@@ -87,43 +89,50 @@ function getNewToken(oauth2Client, callback) {
  * @param {Object} token The token to store to disk.
  */
 function storeToken(token) {
-	try {
-		fs.mkdirSync(TOKEN_DIR)
-	} catch(err) {
-		if (err.code != 'EEXIST') {
-			throw err
-		}
-	}
-	fs.writeFile(TOKEN_PATH, JSON.stringify(token))
-	console.log('Token stored to ' + TOKEN_PATH)
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), () =>
+    console.log('Token stored to ' + TOKEN_PATH)
+  );
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json())
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
 
-	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET')
-		return res.status(200).json({})
-	}
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
+    return res.status(200).json({});
+  }
 
-	next()
-})
+  next();
+});
 
-app.get('/', function (req, res) {
-	res.json({ message: 'success' })
-})
+app.get('/', function(req, res) {
+  res.json({message: 'success'});
+});
 
-app.get('/api/v1/:type/:star', function (req, res) {
-	authorize(credentials, sheets.writeRating, req.params.type, parseInt(req.params.star))
-	res.json({ message: 'success' })
-})
+app.get('/api/v1/:type/:star', function(req, res) {
+  authorize(
+    credentials,
+    sheets.writeRating,
+    req.params.type,
+    parseInt(req.params.star)
+  );
+  res.json({message: 'success'});
+});
 
-
-
-app.listen(port, '0.0.0.0', function () {
-	console.log('Node is running on port: ', port)
-})
+app.listen(port, '0.0.0.0', function() {
+  console.log('Node is running on port: ', port);
+});
